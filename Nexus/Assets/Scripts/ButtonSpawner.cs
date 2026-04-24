3e176bb3445f06f63b37ce65adcb1ad3a121e30b
labgitmomo-unal <labgit.momo@gmail.com>
2026-04-24 17:13:02 -0500

    Implementacion Funcionalidad de Botones en Panel 1
    se logra renderizar los textos que se dan a partir del inspector

--- Nexus/Assets/Scripts/ButtonSpawner.cs
+++ Nexus/Assets/Scripts/ButtonSpawner.cs
@@ -1,34 +1,46 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using TMPro;
+
+[System.Serializable]
+public class BotonData
+{
+    public string texto;
+    public float ponderacion;
+}
 
 public class ButtonSpawner : MonoBehaviour
 {
-    public GameObject buttonPrefab; // tu prefab
-    public Transform panel; // el panel donde van
+    public GameObject buttonPrefab;
+    public Transform panel;
 
-    public int cantidad = 5;
+    public List<BotonData> botones = new List<BotonData>();
 
     void Start()
     {
-        for (int i = 0; i < cantidad; i++)
+        for (int i = 0; i < botones.Count; i++)
         {
+            Debug.Log("Creando botón " + i + " | Texto: " + botones[i].texto + " | Ponderación: " + botones[i].ponderacion);
             GameObject btn = Instantiate(buttonPrefab, panel);
 
-            // Cambiar texto del botón
-            Text txt = btn.GetComponentInChildren<Text>();
+            // Asignar texto
+            TextMeshProUGUI txt = btn.GetComponentInChildren<TextMeshProUGUI>();
             if (txt != null)
             {
-                txt.text = "Botón " + (i + 1);
+                txt.text = botones[i].texto;
             }
 
-            // Añadir evento
+            // Capturar datos del botón
             int index = i;
-            btn.GetComponent<Button>().onClick.AddListener(() => ClickBoton(index));
+            float ponderacion = botones[i].ponderacion;
+
+            btn.GetComponent<Button>().onClick.AddListener(() => ClickBoton(index, ponderacion));
         }
     }
 
-    void ClickBoton(int i)
+    void ClickBoton(int i, float ponderacion)
     {
-        Debug.Log("Click en botón " + i);
+        Debug.Log("Click en botón " + i + " | Ponderación: " + ponderacion);
     }
 }
