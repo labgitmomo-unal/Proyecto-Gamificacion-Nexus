f5932dd56350b47eeb7d23f10dc8369a0da43e6f
labgitmomo-unal <labgit.momo@gmail.com>
2026-06-01 08:52:09 -0500

    Actualizacon Temporizador Panel #1
    Implementacion de transicion de color

--- Nexus/Assets/Scripts/Timer.cs
+++ Nexus/Assets/Scripts/Timer.cs
@@ -9,13 +9,16 @@ public class Timer : MonoBehaviour
 {
     [SerializeField] int min, seg;
     [SerializeField] TextMeshProUGUI  tiempo;
+    [SerializeField] private Image background;
+    private float Max_Time;
     
     private float remaining;
     private bool Under_Way;
 
     private void OnEnable()
     {
-        remaining = min * 60 + seg;
+        Max_Time = min * 60 + seg;
+        remaining = Max_Time;
         Under_Way = true;
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -27,16 +30,40 @@ public class Timer : MonoBehaviour
         if (Under_Way)
         {
             remaining -= Time.deltaTime;
+            UpdateColor();
             int minutes = Mathf.FloorToInt(remaining / 60);
             int seconds = Mathf.FloorToInt(remaining % 60);
             tiempo.text = string.Format("{0:00}:{1:00}", minutes, seconds);
 
             if (remaining <= 0)
             {
-                Under_Way = false;
-                tiempo.text = "00:00";
-                // Aquí puedes agregar cualquier acción que quieras realizar cuando el tiempo se agote
+                Ender_Time();
             }
+        }
+    }
+    private void Ender_Time()
+    {
+        Under_Way = false;
+        tiempo.text = "00:00";
+    }
+
+    private void UpdateColor()
+    {
+        float percentage = remaining / Max_Time;
+
+        if (percentage > 0.5f)
+        {
+            float t = (1f - percentage) * 2f;
+
+            background.color =
+                Color.Lerp(Color.green, Color.yellow, t);
+        }
+        else
+        {
+            float t = (0.5f - percentage) * 2f;
+
+            background.color =
+                Color.Lerp(Color.yellow, Color.red, t);
         }
     }
 }
